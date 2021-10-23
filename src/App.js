2e9d624418be2c0e633a9d85e7932b1f91d1edb7
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2021-10-23 15:06:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MoviesSearch from './MoviesSearch';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <MoviesSearch/>
+    {/* <div className='searchBar'>
+                <input type='text'/>
+            </div> */}
+    </div>
+  );
+}
+
+export default App;
